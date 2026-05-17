9c66d32d7904f530d5d1024cc9296afa00751d5d
SREENATH G <sreenath2410530@ssn.edu.in>
2026-05-17 22:36:27 +0530

    Updated Map details

--- quickconnect-frontend/src/pages/VolunteersPage/VolunteersPage.jsx
+++ quickconnect-frontend/src/pages/VolunteersPage/VolunteersPage.jsx
@@ -2,12 +2,17 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 
-import { fetchSingleVolunteer } from "../../api";
+import { fetchSingleVolunteer, fetchAssignments } from "../../api";
 import MapView from "../../components/MapView/MapView";
 
 export default function VolunteerPage() {
     const navigate = useNavigate();
     const [volunteer, setVolunteer] = useState(null);
+    const [assigned, setAssigned] = useState([]);
+
+    const loadData = () => {
+        fetchAssignments().then(d => setAssigned(d.data));
+      };
 
     useEffect(() => {
     const token = localStorage.getItem("token");
@@ -22,7 +27,7 @@ export default function VolunteerPage() {
             .then(data => setVolunteer(data.data))
             .catch(err => console.error(err));
 
-        console.log(volunteer);
+        loadData();
 
     }, []);
 
@@ -37,14 +42,19 @@ export default function VolunteerPage() {
             <h1>Volunteer Page</h1>
             {volunteer && (
                 <>
-                    <p>{volunteer.name}</p>
-                    <p>{volunteer.email}</p>
+                <div>
+                    <p>Name: {volunteer.name}</p>
+                    <p>Email: {volunteer.email}</p>
+                </div>
                 </>
             )}
             <button onClick={logout}>Logout</button>
         </div>
-        <div>
-            <MapView />
+        <div className="map">
+            <MapView
+                assigned={assigned}
+                volunteers={volunteer}
+            />
         </div>
         
     </>
