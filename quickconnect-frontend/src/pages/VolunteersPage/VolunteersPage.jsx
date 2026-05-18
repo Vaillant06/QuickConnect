1c19a9d8b0f212e599c1a7b25fa27b4b82e71226
SREENATH G <sreenath2410530@ssn.edu.in>
2026-05-18 07:44:49 +0530

    Debugging Assignment for specific Volunteer route

--- quickconnect-frontend/src/pages/VolunteersPage/VolunteersPage.jsx
+++ quickconnect-frontend/src/pages/VolunteersPage/VolunteersPage.jsx
@@ -4,7 +4,7 @@ import { jwtDecode } from "jwt-decode";
 
 import {
     fetchSingleVolunteer,
-    fetchVolunteerAssignments,
+    fetchAssignments,
     fetchNeeds
 } from "../../api";
 
@@ -37,7 +37,7 @@ export default function VolunteerPage() {
                 .then(data => setVolunteer(data.data))
                 .catch(err => console.error(err));
 
-            fetchVolunteerAssignments(volunteerId)
+            fetchAssignments(volunteerId)
                 .then(data => setAssigned(data.data))
                 .catch(err => console.error(err));
             
@@ -104,7 +104,7 @@ export default function VolunteerPage() {
                 <MapView
                     assigned={assigned}
                     volunteers={volunteer ? [volunteer] : []}
-                    needs={needs}
+                    needs={needs}   
                 />
             </div>
         </>
